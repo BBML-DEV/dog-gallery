7e287672e225e0113bc5b5d84be6ac80402891f0
Paulo Vitor <pv.marques2010@hotmail.com>
2023-07-31 14:13:56 -0300

    refactor: Alteração no componente de INPUT

--- src/app/shared/components/Input/index.tsx
+++ src/app/shared/components/Input/index.tsx
@@ -2,22 +2,25 @@ interface InputProps {
   value: string
   name: string
   type: string
+  label: string
   setValue: React.Dispatch<SetStateAction<string>>
 }
 
-export const Input = ({ value, name, type, setValue }: InputProps) => {
+export const Input = ({ label, value, name, type, setValue }: InputProps) => {
   const handleChange = ({ target }: { target: HTMLInputElement }) => {
     setValue(target.value)
   }
 
   return (
-    <input
-      placeholder="teste"
-      className="p-2 bg-zinc-100 w-full rounded-md border-0 hover:ring hover:ring-amber-200 hover:bg-white focus:outline-none focus:ring focus:ring-amber-200 focus:bg-white"
-      value={value}
-      name={name}
-      type={type}
-      onChange={handleChange}
-    />
+    <div className="mb-4">
+      <label htmlFor={name}>{label}</label>
+      <input
+        className="p-2 bg-zinc-100 w-full rounded-md border-0 hover:inputBorder focus:inputBorder"
+        value={value}
+        id={name}
+        type={type}
+        onChange={handleChange}
+      />
+    </div>
   )
 }
